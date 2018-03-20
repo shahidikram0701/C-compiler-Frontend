e016e2e2ec1bce444fdb5a66217774eda623bf9e
shahidikram0701 <shahidikram0701@gmail.com>
2018-03-21 00:27:43 +0530

    updated test case

--- hello_world.cpp
+++ hello_world.cpp
@@ -5,8 +5,11 @@ using namespace std;
 #define b (2/3)
 
 int main(){
-	// Comment
-	int sum = 10+a
+	// single line Comment
+	/*
+	Multi line Comment
+	*/
+	int sum = 10 + a; // Hello
 	int something = b*5; (a + b);
-	return 0; 
+	return 0;
 }
